845f3bf5c5de1abbecada0b49697249f266a1f79
steven nevins <steven.nevins.js@gmail.com>
2023-04-21 21:55:23 -0400

    remove comment

--- src/examples/LinearNFT.sol
+++ src/examples/LinearNFT.sol
@@ -9,8 +9,8 @@ import {toDaysWadUnsafe} from "solmate/utils/SignedWadMath.sol";
 import {LinearSEA} from "src/LinearSEA.sol";
 
 contract LinearNFT is ERC721, LinearSEA {
-    uint256 public totalSold; // The total number of tokens sold so far.
-    uint256 public immutable startTime = block.timestamp; // When VRGDA sales begun.
+    uint256 public totalSold;
+    uint256 public immutable startTime = block.timestamp;
 
     constructor() ERC721("NFT", "LINEAR") LinearSEA(69.42e18, 0.31e18, 2e18) {}
 
